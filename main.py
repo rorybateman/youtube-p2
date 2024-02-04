f6c123e90f9df597159d56687f7daa299046d3d9
rorybateman <70752417+rorybateman@users.noreply.github.com>
2024-02-04 19:35:33 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
+import mpu6050
 import time
-from sys import stdin
-import uselect
 
-csv_filename = "data.csv"
+# Create a new Mpu6050 object
+mpu6050 = mpu6050.mpu6050(0x68)
 
-def save_to_csv(data):
-  with open(csv_filename, "a") as f:
-    f.write(data + "\n")
+# Define a function to read the sensor data
+def read_sensor_data():
+    # Read the accelerometer values
+    accelerometer_data = mpu6050.get_accel_data()
 
-while True:
-  select_result = uselect.select([stdin], [], [], 0)
-  buffer = ''
-  while select_result[0]:
-    input_character = stdin.read(1)
-    if input_character != ',':
-        buffer += input_character
-    else:
-        save_to_csv(buffer)
-        buffer = ''
-    select_result = uselect.select([stdin], [], [], 0)
+    # Read the gyroscope values
+    gyroscope_data = mpu6050.get_gyro_data()
+
+    return accelerometer_data, gyroscope_data
+
+# Start a while loop to continuously read the sensor data
+t0 = time.time()
+# Wait for MPU to Settle
+settling_time = 4
+print('Settling MPU for %d seconds' % settling_time)
+time.sleep(4)
+print('MPU is Done Settling')
+
+
+def get_gyro():
+    gyroscope_data = mpu6050.get_gyro_data()
+    gx= gyroscope_data.get('x')
+    gy= gyroscope_data.get('y')
+    gz= gyroscope_data.get('z')
+    return gx, gy, gz
+
+while true:
+  print(get_gyro())
+  time.sleep(0.5)
+#test
