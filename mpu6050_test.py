37b0aee1d63dd6f64e632c4dc15dd999bc10d2ea
rorybateman <70752417+rorybateman@users.noreply.github.com>
2024-02-04 19:15:12 +0000

    Update mpu6050_test.py

--- mpu6050_test.py
+++ mpu6050_test.py
@@ -26,7 +26,9 @@ while True:
     # Print the sensor data
     print("Accelerometer data:", accelerometer_data)
     print("Gyroscope data:", gyroscope_data)
-    print("Gyroscope data:", type(gyroscope_data))
+    print("Gyroscope data:", gyroscope_data[0])
+    print("Gyroscope data:", gyroscope_data[1])
+    print("Gyroscope data:", gyroscope_data[2])
     print("Temp:", temperature)
 
     # Wait for 1 second
